62056d456b461524d8a09b8ad069ccf18f2bc090
Johannes Urban <johannes.p.urban@gmail.com>
2023-02-22 13:10:33 +0100

    added zero column removal to cleanup.py

--- selfdest_toolkit/data_tools/cleaning.py
+++ selfdest_toolkit/data_tools/cleaning.py
@@ -4,4 +4,22 @@ import numpy as np
 def clean_numpy_data(
         data: np.ndarray
 ) -> np.ndarray:
+    """
+    Function that cleans data by doing some operations. Currently implemented: remove zero columns (features).
+
+    Parameters
+    ----------
+    data : np.ndarray
+        Data which is to be cleaned column wise.
+
+    Returns
+    -------
+    data : np.ndarray
+        cleaned data
+    """
+
+    # remove zero columns
+    data = data[:, np.invert((data == 0).all(axis=0))]
+
+    # return last stage of data
     return data
