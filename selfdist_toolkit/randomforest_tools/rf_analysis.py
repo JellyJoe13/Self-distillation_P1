fa8aeee663e4e86dc2e03de86d3c69c614b6048a
Johannes Urban <johannes.p.urban@gmail.com>
2023-02-24 11:35:53 +0100

    wrote docstring for helper functions

--- selfdist_toolkit/randomforest_tools/rf_analysis.py
+++ selfdist_toolkit/randomforest_tools/rf_analysis.py
@@ -4,6 +4,20 @@ import typing
 def convert_acc_dict(
         acc_dict: typing.Dict[str, typing.List[float]]
 ) -> typing.Dict[str, float]:
+    """
+    Function that computes the mean accuracy score of the measured attempts.
+
+    Parameters
+    ----------
+    acc_dict : typing.Dict[str, typing.List[float]]
+        Dictionary containing lists of measured accuracy scores
+
+    Returns
+    -------
+    typing.Dict[str, float]
+        Dictionary containing the mean accuracy scores computed with this function
+    """
+
     # create placeholder dict
     output = {}
 
@@ -16,7 +30,26 @@ def convert_acc_dict(
     return output
 
 
-def compare_accuracy_dict(normal, sd):
+def compare_accuracy_dict(
+        normal: typing.Dict[str, float],
+        sd: typing.Dict[str, float]
+) -> typing.Dict[str, float]:
+    """
+    Function that computes the difference in accuracy between the two input dicts.
+
+    Parameters
+    ----------
+    normal : typing.Dict[str, float]
+        Dictionary containing the accuracy scores of the normal test
+    sd : typing.Dict[str, float]
+        Dictionary containing the accuracy scores of the self distillation test
+
+    Returns
+    -------
+    dict_diff : typing.Dict[str, float]
+        Dictionary containing the difference in accuracy scores between the input dictionaries
+    """
+
     # generate comparison dict
     comparison_dict = {}
 
