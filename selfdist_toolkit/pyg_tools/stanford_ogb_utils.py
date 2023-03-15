fc571b482c3ff8527e29a427b1c3b0506e2b5f88
Johannes Urban <johannes.p.urban@gmail.com>
2023-03-15 15:12:24 +0100

    wrote comments and documentation

--- selfdist_toolkit/pyg_tools/stanford_ogb_utils.py
+++ selfdist_toolkit/pyg_tools/stanford_ogb_utils.py
@@ -14,37 +14,97 @@ num_embeddings_bond = [5, 6, 2]
 
 
 class GINConvOGB(MessagePassing):
-    def __init__(self, emb_dim):
-        '''
-            emb_dim (int): node embedding dimensionality
-        '''
+    """
+    Original source: https://github.com/snap-stanford/ogb/blob/master/examples/graphproppred/mol/conv.py
+    """
+    def __init__(
+            self,
+            emb_dim: int
+    ):
+        """
+        Initializing function of GINConvOGB.
 
+        Parameters
+        ----------
+        emb_dim : int
+            node embedding dimensionality - dimension the input data is stretched or compressed into
+        """
+
+        # super class method call
         super(GINConvOGB, self).__init__(aggr="add")
 
+        # initialize machine learning object just like in GINConv from pytorch geometric
         self.mlp = torch.nn.Sequential(
+            # linear layer
             torch.nn.Linear(emb_dim, 2 * emb_dim),
+            # norm
             torch.nn.BatchNorm1d(2 * emb_dim),
+            # relu
             torch.nn.ReLU(), torch.nn.Linear(2 * emb_dim, emb_dim))
+
+        # parameter for eps learning
         self.eps = torch.nn.Parameter(torch.Tensor([0]))
 
+        # bond encoder to transfer edge attribute (and node attributes) to a common dimension
         self.bond_encoder = BondEncoder(emb_dim=emb_dim)
 
-    def forward(self, x, edge_index, edge_attr):
+    def forward(
+            self,
+            x: torch.Tensor,
+            edge_index: torch.Tensor,
+            edge_attr: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Function that implements GIN convolution functionality with the addition of using edge attributes
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            input node attributes/embedding
+        edge_index : torch.Tensor
+            input edge indexes
+        edge_attr : torch.Tensor
+            input edge attributes
+
+        Returns
+        -------
+        out : torch.Tensor
+            result of applying GIN Convolution using edge attributes
+        """
+
+        # generate edge embedding (same dimension as node embedding)
         edge_embedding = self.bond_encoder(edge_attr)
+
+        # calculate the output using GIN Convolution logic with the addition of using edge attributes
         out = self.mlp((1 + self.eps) * x + self.propagate(edge_index, x=x, edge_attr=edge_embedding))
 
+        # return result
         return out
 
-    def message(self, x_j, edge_attr):
+    def message(
+            self,
+            x_j,
+            edge_attr
+    ):
         return torch.nn.functional.relu(x_j + edge_attr)
 
-    def update(self, aggr_out):
+    def update(
+            self,
+            aggr_out
+    ):
         return aggr_out
 
 
 class AtomEncoder(torch.nn.Module):
+    """
+    Original source:
+    https://github.com/snap-stanford/ogb/blob/d37cffa2e2cde531ca7b7e75800d331ed1e738a6/ogb/graphproppred/mol_encoder.py
+    """
 
-    def __init__(self, emb_dim):
+    def __init__(
+            self,
+            emb_dim
+    ):
         super(AtomEncoder, self).__init__()
 
         self.atom_embedding_list = torch.nn.ModuleList()
@@ -54,7 +114,10 @@ class AtomEncoder(torch.nn.Module):
             torch.nn.init.xavier_uniform_(emb.weight.data)
             self.atom_embedding_list.append(emb)
 
-    def forward(self, x):
+    def forward(
+            self,
+            x
+    ):
         x_embedding = 0
         for i in range(x.shape[1]):
             x_embedding += self.atom_embedding_list[i](x[:, i])
@@ -63,8 +126,15 @@ class AtomEncoder(torch.nn.Module):
 
 
 class BondEncoder(torch.nn.Module):
+    """
+    Original source:
+    https://github.com/snap-stanford/ogb/blob/d37cffa2e2cde531ca7b7e75800d331ed1e738a6/ogb/graphproppred/mol_encoder.py
+    """
 
-    def __init__(self, emb_dim):
+    def __init__(
+            self,
+            emb_dim
+    ):
         super(BondEncoder, self).__init__()
 
         self.bond_embedding_list = torch.nn.ModuleList()
@@ -74,7 +144,10 @@ class BondEncoder(torch.nn.Module):
             torch.nn.init.xavier_uniform_(emb.weight.data)
             self.bond_embedding_list.append(emb)
 
-    def forward(self, edge_attr):
+    def forward(
+            self,
+            edge_attr
+    ):
         bond_embedding = 0
         for i in range(edge_attr.shape[1]):
             bond_embedding += self.bond_embedding_list[i](edge_attr[:, i])
