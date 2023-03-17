b249dd5c168033232c7e007c3bb15c05c1af1c35
Johannes Urban <johannes.p.urban@gmail.com>
2023-03-17 09:45:34 +0100

    updated function for self distillation elements choice, modification and prediction

--- selfdist_toolkit/pyg_tools/execution.py
+++ selfdist_toolkit/pyg_tools/execution.py
@@ -2,6 +2,8 @@ import torch
 import torch_geometric
 import typing
 from tqdm import tqdm
+import numpy as np
+
 
 # code inspired by https://github.com/snap-stanford/ogb/blob/master/examples/graphproppred/mol/main_pyg.py and
 #
@@ -12,11 +14,13 @@ def training(
 ):
     return None
 
+
 def validating(
         GNN: torch.Moduel,
         validation_data
 ):
     return None
+
 
 def testing(
         GNN: torch.Module,
@@ -24,12 +28,17 @@ def testing(
 ):
     return None
 
-def self_distillation_procedure(
+
+def self_distillation_procedure_soft(
         model: torch.Module,
         self_distillation_data: typing.List[torch_geometric.data.data.Data],
         number_to_pick: int,
-        device
-):
+        device,
+        generation_mode: "actual"  # other: corrected
+) -> typing.Tuple[
+    typing.List[torch_geometric.data.data.Data],
+    typing.List[torch_geometric.data.data.Data]
+]:
     # set the model to evaluation mode
     model.eval()
 
@@ -38,7 +47,6 @@ def self_distillation_procedure(
 
     # iterate over batches
     for batch in tqdm(torch_geometric.loader.DataLoader(self_distillation_data)):
-
         # transfer batch to device
         batch = batch.to(device)
 
@@ -52,4 +60,49 @@ def self_distillation_procedure(
     # fuze data
     prediction = torch.cat(prediction, dim=0).numpy()
 
-    return None
+    # calculate the absolute difference between the values for each of the 2 classes
+    # the larger this value the more secure the prediction is
+    diff = np.abs(prediction[:, 0] - prediction[:, 1])
+
+    # sort the differences
+    diff_srt_idx = np.argsort(diff)
+
+    # choose the <number_to_pick> elements with the largest difference (most secure elements)
+    picked_elements_idx = diff_srt_idx[-number_to_pick:]
+
+    # ==================================================================================================================
+    # create self distillation elements
+
+    # initialize return list
+    self_dist_list = []
+
+    # iterate over indexes = self distillation elements
+    for idx in picked_elements_idx:
+        # pick current data
+        current_data = self_distillation_data[idx]
+
+        # change label in data object
+        if generation_mode == "actual":
+
+            # input the actual prediction as a label
+            current_data.y = torch.tensor(prediction[idx], dtype=torch.float)
+
+        elif generation_mode == "corrected":
+
+            # input the estimated class as a correct label
+            current_data.y = torch.tensor([1., 0.], dtype=torch.float) if np.argmax(prediction[idx]) == 0 \
+                else torch.tensor([0., 1.])
+
+        else:
+            raise Exception("Invalid generation mode parameter supplied. Either actual or corrected are possible modes")
+
+        # append data element to list
+        self_dist_list.append(current_data)
+
+    # calculate list of elements that remain unclassified
+    mask = np.ones(self_distillation_data.shape, dtype=bool)
+    mask[picked_elements_idx] = False
+    unclassified_list = self_distillation_data[mask]
+
+    # return elements and list of remaining unclassified self distillation elements
+    return self_dist_list, unclassified_list
